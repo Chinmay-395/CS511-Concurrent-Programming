56fd5ecec2c1991b0175ba359e85341d218aa740
Chinmay Dali <chinmaydali395@gmail.com>
2022-10-05 11:48:26 -0400

    some comments

--- EB5/ex4.groovy
+++ EB5/ex4.groovy
@@ -12,45 +12,47 @@ mutex = new Semaphore(1);
 def make_routine(int no_exercises) { // returns a random routine
     Random rand = new Random();
     int size = rand.nextInt(no_exercises);
-    print(size)
-    // Every routine will have a apparatus and weight at which it is suppose to be used
     routine = [];
-
+    print("THE SIZE "+size + "\n")
     size.times {
         routine.add(new Tuple(rand.nextInt(4), rand.nextInt(MAX_WEIGHTS)));
     }
-    print(routine);
     return routine;
 }
-make_routine(20);
-// 100.times {
-//     Thread.start { // Client
-//         // enter gym
-//         capacity.acquire();
 
-//         routine = make_routine(20); // random routine of 20 exercises
-//         // complete routine
+/* print("THE SIZE "+size + "\n") THE SIZE 2
+    print(make_routine(5));    [[1, 3], [0, 9]] (Apparatus * max_weight)
+*/
+100.times {
+    Thread.start { // Client
+        // enter gym
+        capacity.acquire();
 
-//         routine.size().times {
-//             int id = it;
-//             apparatus[routine[id].get(0)].acquire();
+        routine = make_routine(5); // random routine of 20 exercises
+        // complete routine
+        print("the routine size " +routine.size() + "\n")
+        2.times {
+            print("THE ITERATION "+it+"\n")
+
+            int id = it;
+            apparatus[routine[id].get(0)].acquire();
             
-//             mutex.acquire();
-//             routine[id].get(1).times {
-//                 weights.acquire();
-//             }
-//             mutex.release();
-//             // exercise
+            mutex.acquire();
+            routine[id].get(1).times {
+                weights.acquire();
+            }
+            mutex.release();
+            // exercise
 
-//             routine[id].get(1).times {
-//                 weights.release();
-//             }
+            routine[id].get(1).times {
+                weights.release();
+            }
 
-//             apparatus[routine[id].get(0)].release();
-//         }
+            apparatus[routine[id].get(0)].release();
+        }
 
-//         capacity.release();
-//     }
-// }
+        capacity.release();
+    }
+}
 
-// return;
+return;
