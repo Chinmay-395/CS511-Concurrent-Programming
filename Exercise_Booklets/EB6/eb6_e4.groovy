4c6dcc3cadbdfe527c093756687eb6dff5a6f491
Chinmay Dali <chinmaydali395@gmail.com>
2022-10-12 14:19:24 -0400

    proficient method for ex4

--- Exercise_Booklets/EB6/eb6_e4.groovy
+++ Exercise_Booklets/EB6/eb6_e4.groovy
@@ -1,50 +1,161 @@
+/*
+ Quiz 5A - 15 Oct 2021
+
+ Name1: Ming Lin	
+ Name2: Connor Haaf
+ Pledge: I pledge my honor that i have abided by the stevens honor system
+
+ */
 import java.util.concurrent.locks.*;
 
+// declarations
 class TrainStation {
+    int ntc=0; // for printing state //north-train-container
+    int stc=0; // for printing state //south-train-container
 
-    void acquireNorthTrackP(){
+    boolean nt=false;
+    boolean st=false;
 
+    // declare locks and condition variables
+	Lock lock = new ReentrantLock();
+	Condition north = lock.newCondition();
+	Condition south = lock.newCondition();
+	Condition freight = lock.newCondition();
+    
+    void acquireNorthTrackP() {
+		lock.lock();
+		println("Acquire N-T-P")
+        try {
+	    	// complete
+			while (nt) north.await(); 
+			nt=true;
+
+	    	ntc++;
+	    	printState();
+			} finally {
+	    	lock.unlock();
+      }
     }
 
-    void releaseNorthTrackP(){
+    void releaseNorthTrackP() {
+			lock.lock();
+			println("Release N-R-P")
+      try {
 
+	    	// complete
+				nt=false;
+				north.signal();
+				freight.signal();
+
+	    	ntc--;
+	    	printState();
+			} finally {
+	    	lock.unlock();
+      }
     }
 
-    void acquireSouthTrackP(){
+    void acquireSouthTrackP() {
+			lock.lock();
+			println("Acquire S-T-P")
+      try {
+	    	// complete
+				while(st) south.await();
+				st=true;
 
+	    	stc++;
+	    	printState();
+			} finally {
+	    	lock.unlock();
+      }
     }
 
-    void releaseSouthTrackP(){
+    void releaseSouthTrackP() {
+			lock.lock();
+			println("Acquire R-T-P")
+      try {
+	    	// complete
+				st=false;
+				south.signal();
+				freight.signal();
 
+	    	stc--;
+	    	printState();
+			} finally {
+	    	lock.unlock();
+      }
     }
 
-    void acquireTracksF(){
+    void acquireTracksF() {
+			lock.lock();
+      try {
+	    	// complete
+				while(nt || st) freight.await();
 
+				nt = true;
+				st = true;
+
+	    	ntc++;
+	    	stc++;
+	    	printState();
+			} finally {
+	    	lock.unlock();
+      }
     }
 
-    void releaseTracksF(){
+    void releaseTracksF() {
+			lock.lock();
+      try {
+	    	// complete
+				freight.signal();
+				north.signal();
+				south.signal();
 
+				nt = false;
+				st = false;
+	    
+	    	ntc--;
+	    	stc--;
+	    	printState();
+			} finally {
+	    	lock.unlock();
+      }
+    }
+
+    void printState() {
+			lock.lock();
+			try {
+				println "ntc: "+ntc+", stc: "+stc;	    
+			} finally {
+				lock.unlock();
+			}
     }
 }
- TrainStation s = new TrainStation ();
- 100. times {
-    Thread.start{ // Passenger Train going North
-        s.acquireNorthTrackP();
-        print " NPT "+ Thread.currentThread().getId();
-        s.releaseNorthTrackP();
+
+TrainStation s = new TrainStation();
+
+5.times {
+    int id = it;
+    Thread.start { // Freight Train going in any direction
+			s.acquireTracksF();
+			print "FT "+ Thread.currentThread().getId()+"\n";
+			s.releaseTracksF()
     }
 }
- 100. times {
-    Thread.start{ // Passenger Train going South
-        s.acquireSouthTrackP();
-        print " SPT "+ Thread.currentThread().getId();
-        s.releaseSouthTrackP()
+
+5.times{
+    int id = it;
+    Thread.start { // Passenger Train going North
+			s.acquireNorthTrackP();
+			print " NPT "+ Thread.currentThread().getId()+"\n";
+			s.releaseNorthTrackP();
     }
 }
- 10. times {
-    Thread.start{ // Freight Train
-        s.acquireTracksF();
-        print "FT "+ Thread.currentThread().getId();
-        s.releaseTracksF();
+
+5.times{
+    int id = it;
+    Thread.start { // Passenger Train going South
+			s.acquireSouthTrackP();
+			print " SPT "+ Thread.currentThread().getId()+"\n";
+			s.releaseSouthTrackP();
     }
 }
