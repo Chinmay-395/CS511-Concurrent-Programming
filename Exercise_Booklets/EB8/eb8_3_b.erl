2331f0610477ef3d8eff89d9a19008a99f176e51
Chinmay Dali <chinmaydali395@gmail.com>
2022-12-13 13:03:13 -0500

    ex3 both done

---
+++ Exercise_Booklets/EB8/eb8_3_b.erl
@@ -0,0 +1,28 @@
+-module(eb8_3_b).
+-compile(nowarn_export_all).
+-compile(export_all).
+
+start() ->
+    S = spawn(?MODULE, server, [0]),
+    [spawn(?MODULE, client, [S])|| _ <- lists:seq(1, 5)].
+
+client(S) ->
+    S!{continue},
+    S!{continue},
+    S!{continue},
+    S!{counter,self()},
+    receive
+        {final,Val} ->
+            io:format("THE VAL ~w ~n",[Val])
+    end.
+    
+server(CounterValue) ->
+    % io:format("THE CURRENT VAL ~w ~n",[CounterValue]),
+    receive
+        {continue} ->
+            server(CounterValue+1);
+        {counter,From} ->
+            % io:format("THE COUNTER ~w ~n",[CounterValue]),
+            From!{final,CounterValue}
+            % server(CounterValue)
+    end.
