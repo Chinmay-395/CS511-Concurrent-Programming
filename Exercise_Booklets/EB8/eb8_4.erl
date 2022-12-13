ecfbda947f0b249ce91875d08b020c6662209aae
Chinmay Dali <chinmaydali395@gmail.com>
2022-12-13 14:17:09 -0500

    ex4 and ex5 of eb8

--- Exercise_Booklets/EB8/eb8_4.erl
+++ Exercise_Booklets/EB8/eb8_4.erl
@@ -1,56 +1,20 @@
--module(eb8_4).
--compile(nowarn_export_all).
+-module(eb8_4_new).
 -compile(export_all).
+start(Freq) ->
+    L = [ spawn(? MODULE , client , [] ) || _ <- lists : seq (1 ,2) ] ,
+    spawn(? MODULE , timer , [ Freq , L ] ).
 
-start() ->
-    S = spawn(?MODULE, timer, [100, []]),
-    C1 = spawn(?MODULE, client1, [S]),
-    C2 = spawn(?MODULE, client2, [S]),
-    C3 = spawn(?MODULE, client3, [S]),
-    C4 = spawn(?MODULE, client4, [S]).
+timer(0,L) ->
+    io:format("ENDED"),
+    ok;
 
-timehelp(Time, L, []) ->
-    receive
-        {From, register} ->
-            NewL = [From] ++ L,
-            timer(Time, NewL)
-    end,
-    timer(Time, L);
-timehelp(Time, L, [H | T]) ->
-    timer:send_after(Time, H, {tick}),
-    timehelp(Time, L, T).
-
-timer(Time, L) ->
-    timehelp(Time, L, L).
-
-client1(S) ->
-    S ! {self(), register},
-    receive
-        {tick} ->
-            io:format("Received a tick ~n")
-    end,
-    client1(S).
-
-client2(S) ->
-    S ! {self(), register},
-    receive
-        {tick} ->
-            io:format("Received a tick ~n")
-    end,
-    client2(S).
-
-client3(S) ->
-    S ! {self(), register},
-    receive
-        {tick} ->
-            io:format("Received a tick ~n")
-    end,
-    client3(S).
-
-client4(S) ->
-    S ! {self(), register},
-    receive
-        {tick} ->
-            io:format("Received a tick ~n")
-    end,
-    client4(S).
+timer ( Freq , L ) when Freq>0->
+    timer:sleep(Freq),
+    [Pid!{Pid,tick}||Pid<-L],
+    timer(Freq-1,L).
+client() ->
+receive
+    {From, tick } ->
+        io:format("THE TICK ~p ~n",[From]),
+        client()
+    end.
